d0c0e164db34e26d1dc0ccfcb1b791ae46fff031
Lis <elis_jasper@estudante.sesisenai.org.br>
2026-03-04 16:17:53 -0300

    codigo arrumado para execução com tomcat manual

--- biblioteca/src/main/java/br/com/centroweg/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/br/com/centroweg/biblioteca/BibliotecaApplication.java
@@ -2,9 +2,16 @@ package br.com.centroweg.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BibliotecaApplication {
+public class BibliotecaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BibliotecaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
--- biblioteca/src/main/java/br/com/centroweg/biblioteca/util/Conexao.java
+++ biblioteca/src/main/java/br/com/centroweg/biblioteca/util/Conexao.java
@@ -5,10 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimezone=UTC";
+
+    private static final String url = "jdbc:mysql://host.docker.internal:3306/biblioteca?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String user = "root";
     private static final String password = "mysqlPW";
-    public static Connection conect() throws SQLException{
+
+    public static Connection conect() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL não encontrado!", e);
+        }
         return DriverManager.getConnection(url, user, password);
     }
 }
